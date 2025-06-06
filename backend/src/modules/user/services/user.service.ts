71f445842c711b2d6f056ed9e9009b5b7c65179c
varguitas <vargasvr94@gmail.com>
2025-06-06 00:33:10 +0000

    [add] log for user not found

--- backend/src/modules/user/services/user.service.ts
+++ backend/src/modules/user/services/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { IUser } from '../interfaces/IUser';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
@@ -6,6 +6,8 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  private readonly logger = new Logger(UserService.name);
+
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
@@ -18,6 +20,7 @@ export class UserService {
   async findById(id: number) {
     const user = await this.usersRepository.findOneBy({ id });
     if (!user) {
+      this.logger.error(`User with id ${id} not found`);
       throw new NotFoundException(`User with id ${id} not found`);
     }
     return user;
